ccec7018af818f68e6ad14f58d0829488582f1d1
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-04-27 20:00:10 +0200

    junit to spock

---
+++ roman-arabic/functional-java/src/test/groovy/org/kata/RomanToArabicNumeralsSpecification.groovy
@@ -0,0 +1,50 @@
+package org.kata
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.util.function.Function
+
+import static org.kata.RomanNumber.romeNumberOf
+
+@Unroll
+class RomanToArabicNumeralsSpecification extends Specification {
+
+    def "converts single rome numeral of #roman to #arabic"() {
+        expect:
+        ArabicNumber arabicNumber = romeNumberOf(roman).to(arabic())
+        arabicNumber.value() == arabic
+
+        where:
+        roman   ||   arabic
+        "I"     ||   1
+        "V"     ||   5
+        "X"     ||   10
+        "L"     ||   50
+        "C"     ||   100
+        "D"     ||   500
+        "M"     ||   1000
+    }
+
+    def "converts multiple rome numerals of #roman to #arabic"() {
+        expect:
+        ArabicNumber arabicNumber = romeNumberOf(roman).to(arabic())
+        arabicNumber.value() == arabic
+
+        where:
+        roman           ||  arabic
+        "IV"            ||  4
+        "VI"            ||  6
+        "III"           ||  3
+        "XCIV"          ||  94
+        "MCMLIV"        ||  1954
+        "MCMXC"         ||  1990
+        "MMXIV"         ||  2014
+        "XXXXXX"        ||  60
+        "MMMDCCCXLIV"   ||  3844
+    }
+
+    static Function<RomanNumber, ArabicNumber> arabic() {
+        new RomanToArabicConverter()
+    }
+}
