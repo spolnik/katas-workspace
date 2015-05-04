dfc6a214c1e2fb97e3d1e840d509faed2cd544ce
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-05-04 11:05:09 +0200

    - simplify algorithm by mapping input List[Char] to List[ArabicNumeral]

--- roman-arabic/scala/src/main/scala/org/kata/RomanNumeral.scala
+++ roman-arabic/scala/src/main/scala/org/kata/RomanNumeral.scala
@@ -3,19 +3,23 @@ package org.kata
 case class RomanNumeral(private val romanNumeral: String) {
 
   def toArabic: ArabicNumeral = {
-    reduce(romanNumeral.toList, ArabicNumeral(0))
+    reduce(
+      romanNumeral.toList
+        .map(toArabicNumeral),
+      result = ArabicNumeral(0)
+    )
   }
 
-  private def reduce(romanNumerals: List[Char], result: ArabicNumeral): ArabicNumeral = romanNumerals match {
-    case first :: second :: rest if arabicOf(first) < arabicOf(second) =>
-      reduce(second :: rest, result - arabicOf(first))
+  private def reduce(romanNumerals: List[ArabicNumeral], result: ArabicNumeral): ArabicNumeral = romanNumerals match {
+    case first :: second :: rest if first < second =>
+      reduce(second :: rest, result - first)
     case single :: Nil =>
-      result + arabicOf(single)
+      result + single
     case first :: rest =>
-      reduce(rest, result + arabicOf(first))
+      reduce(rest, result + first)
   }
 
-  private def arabicOf(value: Char) = value match {
+  private def toArabicNumeral(value: Char) = value match {
     case 'I' => ArabicNumeral(1)
     case 'V' => ArabicNumeral(5)
     case 'X' => ArabicNumeral(10)
