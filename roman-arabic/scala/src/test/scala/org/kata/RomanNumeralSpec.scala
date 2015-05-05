0db22271d66e1384f6669ad45d697ac4b6835048
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-05-05 14:38:30 +0200

    - add validation + test for it - add two more failure cases

--- roman-arabic/scala/src/test/scala/org/kata/RomanNumeralSpec.scala
+++ roman-arabic/scala/src/test/scala/org/kata/RomanNumeralSpec.scala
@@ -7,43 +7,46 @@ class RomanNumeralSpec extends FlatSpec with Matchers {
 
   val scenarios =
     Table(
-      ("roman",       "arabic"),
-      ("I",           1),
-      ("V",           5),
-      ("X",           10),
-      ("L",           50),
-      ("C",           100),
-      ("D",           500),
-      ("M",           1000),
-      ("IV",          4),
-      ("VI",          6),
-      ("III",         3),
-      ("XCIV",        94),
-      ("MCMLIV",      1954),
-      ("MCMXC",       1990),
-      ("MMXIV",       2014),
+      ("roman", "arabic"),
+      ("I", 1),
+      ("V", 5),
+      ("X", 10),
+      ("L", 50),
+      ("C", 100),
+      ("D", 500),
+      ("M", 1000),
+      ("IV", 4),
+      ("VI", 6),
+      ("III", 3),
+      ("XCIV", 94),
+      ("MCMLIV", 1954),
+      ("MCMXC", 1990),
+      ("MMXIV", 2014),
       ("MMMDCCCXLIV", 3844)
     )
 
 
   "A roman numeral" should "be converted to arabic numeral" in {
 
-    forAll (scenarios) { (romanValue, arabic) =>
-      RomanNumeral(romanValue).toArabic should be (arabic)
+    forAll(scenarios) { (romanValue, arabic) =>
+      RomanNumeral(romanValue).toArabic should be(arabic)
     }
   }
 
   val failureScenarios =
     List(
       "IIII",
+      "DD",
+      "LL",
+      "VV",
       "IIV",
       "IXIX",
-    "XXXXXX"
+      "XXXXXX"
     )
 
-  "An incorrect roman numeral" should "throw exception" in {
+  "An incorrect roman numeral" should "throw exception IllegalArgumentException" in {
 
-    failureScenarios.foreach( roman =>
+    failureScenarios.foreach(roman =>
       intercept[IllegalArgumentException] {
         RomanNumeral(roman).toArabic
       }
