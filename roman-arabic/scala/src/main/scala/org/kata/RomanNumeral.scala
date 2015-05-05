9ff02fa6bcbe4beb742b0078416c31e1493ce449
Jacek Spolnik <jacek.spolnik@gmail.com>
2015-05-05 14:40:08 +0200

    - make 3999 as the max number

--- roman-arabic/scala/src/main/scala/org/kata/RomanNumeral.scala
+++ roman-arabic/scala/src/main/scala/org/kata/RomanNumeral.scala
@@ -14,7 +14,7 @@ case class RomanNumeral(private val romanNumeral: String) {
     )
   }
 
-  private val correctRomanNumeralPattern = "^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$".r
+  private val correctRomanNumeralPattern = "^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$".r
 
   private def checkIfValid(romanNumeral: String) =  romanNumeral match {
     case correctRomanNumeralPattern(a,b,c) => true
