0b84661b12fdabf7d653d2726ee0cb88c243ebb4
Jacek Spólnik <jacek.spolnik@gmail.com>
2015-10-06 18:59:52 +0200

    - round 4 - add 3 more tests covering finding numbers in array of 3 elements - rename unit tests to better express the behavior and context they tests

--- karate-chop/java/src/test/java/com/codekata/karatechop/BinarySearchSpec.java
+++ karate-chop/java/src/test/java/com/codekata/karatechop/BinarySearchSpec.java
@@ -23,15 +23,33 @@ public class BinarySearchSpec {
     }
 
     @Test
-    public void returns_not_found_if_element_is_absent() throws Exception {
+    public void returns_not_found_if_element_is_absent_in_array_of_one_item() throws Exception {
 
         assertThat(binarySearch.chop(3, array(1))).isEqualTo(NOT_FOUND);
     }
 
     @Test
-    public void returns_0_when_we_look_for_the_only_item_in_the_array() throws Exception {
+    public void returns_0_when_we_look_for_the_only_item_in_the_array_of_one_item() throws Exception {
 
         assertThat(binarySearch.chop(1, array(1))).isEqualTo(0);
+    }
+
+    @Test
+    public void returns_0_when_we_look_for_1_in_array_of_1_3_5() throws Exception {
+
+        assertThat(binarySearch.chop(1, array(1,3,5))).isEqualTo(0);
+    }
+
+    @Test
+    public void returns_1_when_we_look_for_3_in_array_of_1_3_5() throws Exception {
+
+        assertThat(binarySearch.chop(3, array(1,3,5))).isEqualTo(1);
+    }
+
+    @Test
+    public void returns_2_when_we_look_for_5_in_array_of_1_3_5() throws Exception {
+
+        assertThat(binarySearch.chop(5, array(1,3,5))).isEqualTo(2);
     }
 
     private int[] array(int... elements) {
