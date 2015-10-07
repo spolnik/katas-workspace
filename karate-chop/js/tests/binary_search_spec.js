047a23659292f98ba0ac26c5b3bd2501d757bda9
Jacek Spólnik <jacek.spolnik@gmail.com>
2015-10-07 18:43:38 +0200

    - round 2 - further refactoring - using chai to use bdd style of tests/asserts

--- karate-chop/js/tests/binary_search_spec.js
+++ karate-chop/js/tests/binary_search_spec.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var DummyBinarySearch = require('../components/DummyBinarySearch');
 
 describe('binary search', () => {
@@ -9,17 +9,17 @@ describe('binary search', () => {
 
     describe('chop(x, [])', () => {
         it('should return -1 when we look for 3 in []', () =>
-                assert.equal(-1, this.binary_search.chop(3, []))
+            expect(this.binary_search.chop(3, [])).to.equal(-1)
         );
     });
 
     describe('chop(x, [1])', () => {
         it('should return -1 when we look for 3 in [1]', () =>
-            assert.equal(-1, this.binary_search.chop(3, [1]))
+            expect(this.binary_search.chop(3, [1])).to.equal(-1)
         );
 
         it('should return 0 when we look for 1 in [1]', () =>
-            assert.equal(0, this.binary_search.chop(1, [1]))
+            expect(this.binary_search.chop(1, [1])).to.equal(0)
         );
     });
 });
