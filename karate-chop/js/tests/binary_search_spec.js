9ab7bf43ae8b0e4c96b1fb8879bdfcb81fc6dd47
Jacek Spólnik <jacek.spolnik@gmail.com>
2015-10-07 18:37:06 +0200

    - round 2 - adding two tests for array with one element - one with looking for existing item, another for absent one

--- karate-chop/js/tests/binary_search_spec.js
+++ karate-chop/js/tests/binary_search_spec.js
@@ -8,8 +8,16 @@ describe('binary search', () => {
     });
 
     describe('chop()', () => {
-        it('should return -1 when we look for 3 in []', () => {
-            assert.equal(-1, this.binary_search.chop(3, []));
-        });
+        it('should return -1 when we look for 3 in []', () =>
+            assert.equal(-1, this.binary_search.chop(3, []))
+        );
+
+        it('should return -1 when we look for 3 in [1]', () =>
+            assert.equal(-1, this.binary_search.chop(3, [1]))
+        );
+
+        it('should return 0 when we look for 1 in [1]', () =>
+            assert.equal(0, this.binary_search.chop(1, [1]))
+        );
     });
 });
