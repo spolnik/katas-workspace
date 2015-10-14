56c2e827df79ff65a2e37e81b1cfb712853b751d
Jacek Spólnik <jacek.spolnik@gmail.com>
2015-10-14 10:34:16 +0200

    - add test for checking if TripService returns empty list of trips for user who is not a friend

--- legacy-code/TripService/src/test/groovy/org/craftedsw/harddependencies/TripServiceSpec.groovy
+++ legacy-code/TripService/src/test/groovy/org/craftedsw/harddependencies/TripServiceSpec.groovy
@@ -1,15 +1,23 @@
 package org.craftedsw.harddependencies
 
 import org.craftedsw.harddependencies.exception.UserNotLoggedInException
+import org.craftedsw.harddependencies.trip.Trip
 import org.craftedsw.harddependencies.user.User
 import org.craftedsw.harddependencies.user.UserSession
 import spock.lang.Specification
 
 class TripServiceSpec extends Specification {
 
+    UserSession userSession
+    List<Trip> trips
+
+    def setup() {
+        userSession = Mock(UserSession)
+    }
+
     def "throws UserNotLoggedInException if there is no user logged in"() {
         setup:
-        def userSession = Mock(UserSession)
+
         userSession.getLoggedUser() >> null
 
         when:
@@ -19,5 +27,18 @@ class TripServiceSpec extends Specification {
         then:
         def e = thrown(UserNotLoggedInException)
         e.cause == null
+    }
+
+    def "returns empty trip list for user who is not a friend"() {
+        setup:
+        def loggedUser = new User()
+        userSession.getLoggedUser() >> loggedUser
+
+        when:
+        def service = new TripService(userSession)
+        trips = service.getTripsByUser(new User())
+
+        then:
+        trips.empty
     }
 }
