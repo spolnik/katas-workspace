4e755d564928d1538f4daf745e3f8b3120817738
Jacek Spólnik <jacek.spolnik@gmail.com>
2015-10-14 10:04:48 +0200

    - add skeleton for legacy code trip service kata

---
+++ legacy-code/TripService/src/main/java/org/craftedsw/harddependencies/TripService.java
@@ -0,0 +1,32 @@
+package org.craftedsw.harddependencies;
+
+import org.craftedsw.harddependencies.exception.UserNotLoggedInException;
+import org.craftedsw.harddependencies.trip.Trip;
+import org.craftedsw.harddependencies.trip.TripDAO;
+import org.craftedsw.harddependencies.user.User;
+import org.craftedsw.harddependencies.user.UserSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripService {
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        List<Trip> tripList = new ArrayList<Trip>();
+        User loggedUser = UserSession.getInstance().getLoggedUser();
+        boolean isFriend = false;
+        if (loggedUser != null) {
+            for (User friend : user.getFriends()) {
+                if (friend.equals(loggedUser)) {
+                    isFriend = true;
+                    break;
+                }
+            }
+            if (isFriend) {
+                tripList = TripDAO.findTripsByUser(user);
+            }
+            return tripList;
+        } else {
+            throw new UserNotLoggedInException();
+        }
+    }
+}
---
+++ legacy-code/TripService/src/main/java/org/craftedsw/harddependencies/trip/Trip.java
@@ -0,0 +1,4 @@
+package org.craftedsw.harddependencies.trip;
+
+public class Trip {
+}
